c52df6f9103d2d9e8e94552f1ed58fe9b730ade8
Haribabu S <haribabu.sucki@gmail.com>
2017-02-06 08:52:16 +0530

    Added api to fetch the project, dashboard details from the Jira instance

---
+++ src/main/java/com/hcl/hawkeye/Exceptions/NoProjectDetailsException.java
@@ -0,0 +1,17 @@
+package com.hcl.hawkeye.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR, reason="Failed to retrieve the project details from the jira")
+public class NoProjectDetailsException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoProjectDetailsException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
--- src/main/java/com/hcl/hawkeye/projectmanagement/controller/ProjectManagementController.java
+++ src/main/java/com/hcl/hawkeye/projectmanagement/controller/ProjectManagementController.java
@@ -1,8 +1,40 @@
 package com.hcl.hawkeye.projectmanagement.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hcl.hawkeye.projectmanagement.DO.DashBoardDetails;
+import com.hcl.hawkeye.projectmanagement.DO.ProjectDetails;
+import com.hcl.hawkeye.projectmanagement.service.ProjectManagementService;
 
 @RestController
 public class ProjectManagementController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ProjectManagementController.class);
+	
+	@Autowired
+	ProjectManagementService pmService;
+	
+	@RequestMapping(value="/getProject/project/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public ProjectDetails getProjectData(@PathVariable("id") int projectId) {
+		logger.info("Rquesting api to get the project details with ID: {}", projectId);
+		ProjectDetails proDetails = pmService.getProjectDetails(projectId);
+		return proDetails;
+	}
+	
+	@RequestMapping(value="/getDashboard", method = RequestMethod.GET)
+	@ResponseBody
+	public DashBoardDetails getDashBoardData() {
+		logger.info("Rquesting api to get the dasboard information");
+		DashBoardDetails dashBInfo = pmService.getDashBoardInfo();
+		return dashBInfo;
+	}
 
 }
