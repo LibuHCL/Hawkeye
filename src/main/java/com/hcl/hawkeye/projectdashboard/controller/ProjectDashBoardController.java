4070facb84cfa687dd3848d07ae73aad2c2ea3d8
phani.imuguri <phani.imuguri@hcl.com>
2017-02-10 14:14:02 +0530

    Cross orign changes

--- src/main/java/com/hcl/hawkeye/projectdashboard/controller/ProjectDashBoardController.java
+++ src/main/java/com/hcl/hawkeye/projectdashboard/controller/ProjectDashBoardController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import com.hcl.hawkeye.projectdashboard.DO.ProjectDashBoard;
 import com.hcl.hawkeye.projectdashboard.service.ProjectDashBoardService;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ProjectDashBoardController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ProjectDashBoardController.class);
--- src/main/java/com/hcl/hawkeye/projectmetrics/controller/ProjectMetricsController.java
+++ src/main/java/com/hcl/hawkeye/projectmetrics/controller/ProjectMetricsController.java
@@ -3,6 +3,7 @@ package com.hcl.hawkeye.projectmetrics.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import com.hcl.hawkeye.projectmetrics.DO.ProjectMetrics;
 import com.hcl.hawkeye.projectmetrics.service.ProjectMetricsService;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ProjectMetricsController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ProjectMetricsController.class);
