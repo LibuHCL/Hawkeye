8775f9103d80750cf152e60346a32734680b1d0b
Haribabu S <haribabu.sucki@gmail.com>
2017-02-10 18:11:14 +0530

    Added Cross origin to controller

--- src/main/java/com/hcl/hawkeye/programingkpis/controller/ProgramIngKPIController.java
+++ src/main/java/com/hcl/hawkeye/programingkpis/controller/ProgramIngKPIController.java
@@ -3,6 +3,7 @@ package com.hcl.hawkeye.programingkpis.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +14,7 @@ import com.hcl.hawkeye.programingkpis.DO.Result;
 import com.hcl.hawkeye.programingkpis.service.ProgramIngKPIService;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ProgramIngKPIController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ProgramIngKPIController.class);
