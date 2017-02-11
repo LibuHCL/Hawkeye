ffe511591a65ef66a233449e90630c140b7c4c9a
phani.imuguri <phani.imuguri@hcl.com>
2017-02-11 10:17:30 +0530

    Cross origin changes

--- src/main/java/com/hcl/hawkeye/codequality/controller/CodeQualityController.java
+++ src/main/java/com/hcl/hawkeye/codequality/controller/CodeQualityController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hcl.hawkeye.codequality.service.CodeQualityService;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class CodeQualityController {
 
 	private static final Logger logger = LoggerFactory
